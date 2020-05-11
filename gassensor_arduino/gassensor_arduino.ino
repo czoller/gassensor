a2926e71f8859ae20f251502ca9506b505f2f9db
czoller <czoller@users.noreply.github.com>
2020-05-11 23:46:26 +0200

    GASES, UNITS und DATA durch , statt = abtrennen

--- gassensor_arduino/gassensor_arduino.ino
+++ gassensor_arduino/gassensor_arduino.ino
@@ -5,14 +5,14 @@ void setup() {
     Serial.begin(115200);  // start serial for output
     gas.begin(0x04);//the default I2C address of the slave is 0x04
     gas.powerOn();
-    Serial.println("GASES=NH3,CO,NO2,C3H8,C4H10,CH4,H2,C2H5OH");
-    Serial.println("UNITS=ppm,ppm,ppm,ppm,ppm,ppm,ppm,ppm");
+    Serial.println("GASES,NH3,CO,NO2,C3H8,C4H10,CH4,H2,C2H5OH");
+    Serial.println("UNITS,ppm,ppm,ppm,ppm,ppm,ppm,ppm,ppm");
 }
 
 void loop() {
     float c;
 
-    Serial.print("DATA=");
+    Serial.print("DATA,");
 
     c = gas.measure_NH3();
     if (c >= 0) {
