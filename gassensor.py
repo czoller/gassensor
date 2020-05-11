e4e8a8cbaf80c44b987d9e15b6a6f1f161cc837a
czoller <czoller@users.noreply.github.com>
2020-05-11 23:47:36 +0200

    Uhrzeit in einzelnen Zeilen ausgeben

--- gassensor.py
+++ gassensor.py
@@ -1,15 +1,20 @@
 import serial
 from datetime import datetime
 
-def logData():
-    logfiletime = datetime.now().strftime("%Y%m%d-%H%M")
-    logfilepath = "logs/gassensor-" + logfiletime + ".log"
-    logfile = open(logfilepath, "w")
-    
+def buildLogFilePath():
+    #logFileTime = datetime.now().strftime("%Y%m%d-%H%M")
+    #return "logs/gassensor-" + logFileTime + ".log"
+    return "logs/gassensor.log"
+
+def logData(logFilePath):
+    logfile = open(logFilePath, "w")
     ser = serial.Serial(COMPORT, BAUDRATE, timeout=0)
     while (1):
         line = ser.readline().decode("utf-8")
         if (line != ""):
+            linetime = datetime.now().strftime("%H:%M:%S")
+            line = line.replace('\n', '\n' + linetime + ',')
+            print(line)
             logfile.write(line)
             
     logfile.close()
@@ -25,7 +30,8 @@ COMPORT = '/dev/ttyACM0';
 BAUDRATE = 115200
 
 def main():
-    logData()
+    logFilePath = buildLogFilePath()
+    logData(logFilePath)
 
 if __name__ == "__main__":
     main()
