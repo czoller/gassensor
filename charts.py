3dbb612f1fc55b2ee3506a3d1542fb351ceb3859
czoller <czoller@users.noreply.github.com>
2020-05-12 23:14:25 +0200

    Alle drei Charts in ein PDF

--- charts.py
+++ charts.py
@@ -1,23 +1,40 @@
+import os
 import argparse
 import pandas as pd
 import numpy as np
 from scipy import stats
 import plotly.graph_objects as go
+from PyPDF2 import PdfFileMerger, PdfFileReader
 
 def createCharts(data, pdfFilePath):
-    createChart(data, pdfFilePath.replace('.pdf', '.0.pdf'), ['NH3', 'NO2', 'H2', 'C2H5OH'])
-    createChart(data, pdfFilePath.replace('.pdf', '.1.pdf'), ['CO'])
-    createChart(data, pdfFilePath.replace('.pdf', '.2.pdf'), ['C3H8', 'C4H10', 'CH4'])
+    file0 = pdfFilePath.replace('.pdf', '.0.pdf')
+    file1 = pdfFilePath.replace('.pdf', '.1.pdf')
+    file2 = pdfFilePath.replace('.pdf', '.2.pdf')
+    createChart(data, file0, ['NH3', 'NO2', 'H2', 'C2H5OH'])
+    createChart(data, file1, ['CO'])
+    createChart(data, file2, ['C3H8', 'C4H10', 'CH4'])
+    joinPdf([file0, file1, file2], pdfFilePath)
     
 def createChart(data, pdfFilePath, gases):
     fig = go.Figure()
     for gas in gases:
-        subdata = data[[gas]]
-        subdata = subdata[(np.abs(stats.zscore(subdata)) < 3).all(axis=1)] 
-        fig.add_trace(go.Scatter(x=subdata.index, y=subdata[gas], name=gas))
+        d = filterData(data, gas) 
+        fig.add_trace(go.Scatter(x=d.index, y=d[gas], name=gas))
     fig.update_yaxes(title_text='ppm')
-    fig.update_layout(margin=dict(l=20, r=20, t=20, b=20))
+    fig.update_layout(margin=dict(l=20, r=20, t=20, b=20), showlegend=True)
     fig.write_image(pdfFilePath)
+    
+def filterData(data, gas):
+    d = data[[gas]]
+    return d[(np.abs(stats.zscore(d)) < 3).all(axis=1)]
+    
+def joinPdf(sourceFiles, destFile):
+    mergedObject = PdfFileMerger(strict=False)
+    for sourceFile in sourceFiles:
+        mergedObject.append(PdfFileReader(sourceFile, 'rb'))
+    mergedObject.write(destFile)
+    for sourceFile in sourceFiles:
+        os.remove(sourceFile)
 
 def readCsvFile(csvfile):
     return pd.read_csv(csvfile, index_col=0, parse_dates=True)
@@ -30,7 +47,6 @@ def parseArguments():
 def main():
     args = parseArguments()
     data = readCsvFile(args.csvfile)
-    print(data)
     pdfFilePath = args.csvfile.replace('.csv', '.pdf')
     createCharts(data, pdfFilePath)
     
