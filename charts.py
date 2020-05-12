0ccf6f0a8d93a2a8ec59997a951f480035c5bd90
czoller <czoller@users.noreply.github.com>
2020-05-12 22:48:59 +0200

    Ausreißer filtern

--- charts.py
+++ charts.py
@@ -1,20 +1,26 @@
 import argparse
 import pandas as pd
+import numpy as np
+from scipy import stats
 import plotly.graph_objects as go
 
 def createCharts(data, pdfFilePath):
-    createChart(data, pdfFilePath, ['NH3', 'NO2', 'H2', 'C2H5OH'])
+    createChart(data, pdfFilePath.replace('.pdf', '.0.pdf'), ['NH3', 'NO2', 'H2', 'C2H5OH'])
+    createChart(data, pdfFilePath.replace('.pdf', '.1.pdf'), ['CO'])
+    createChart(data, pdfFilePath.replace('.pdf', '.2.pdf'), ['C3H8', 'C4H10', 'CH4'])
     
 def createChart(data, pdfFilePath, gases):
     fig = go.Figure()
     for gas in gases:
-        fig.add_trace(go.Scatter(x=data['time'], y=data[gas], name=gas))
+        subdata = data[[gas]]
+        subdata = subdata[(np.abs(stats.zscore(subdata)) < 3).all(axis=1)] 
+        fig.add_trace(go.Scatter(x=subdata.index, y=subdata[gas], name=gas))
     fig.update_yaxes(title_text='ppm')
     fig.update_layout(margin=dict(l=20, r=20, t=20, b=20))
     fig.write_image(pdfFilePath)
 
 def readCsvFile(csvfile):
-    return pd.read_csv(csvfile, parse_dates=[0])
+    return pd.read_csv(csvfile, index_col=0, parse_dates=True)
     
 def parseArguments():
     argParser = argparse.ArgumentParser(description="Erzeugt Graphen von Gassensordaten.");
