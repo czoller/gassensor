0da2c2aaf655e4979010351421f4082db27c11e1
czoller <czoller@users.noreply.github.com>
2020-05-14 14:36:24 +0200

    Aufwärmzeit auf 20 Minuten erhöht

--- gassensor.py
+++ gassensor.py
@@ -101,10 +101,10 @@ def createPlot(data, pdfFilePath, gases, title, markerTime):
     
     shapes = []
     annotations = []
-    if (data.index[-1] - data.index[0]).total_seconds() > 10 * 60:
-        x10min = data.index[0] + timedelta(minutes = 10)
-        shapes.append(dict(type='line', xref='x', yref='paper', x0=x10min, y0=0, x1=x10min, y1=1.1, line=dict(color='grey',dash='dot')))
-        annotations.append(dict(text='Gassensor<br>aufwärmen', x=x10min, y=1.1, xref='x', yref='paper', xanchor='right', align='right', showarrow=False, font=dict(color='grey')))
+    if (data.index[-1] - data.index[0]).total_seconds() > 20 * 60:
+        xHeating = data.index[0] + timedelta(minutes = 20)
+        shapes.append(dict(type='line', xref='x', yref='paper', x0=xHeating, y0=0, x1=xHeating, y1=1.1, line=dict(color='grey',dash='dot')))
+        annotations.append(dict(text='Gassensor<br>aufwärmen', x=xHeating, y=1.1, xref='x', yref='paper', xanchor='right', align='right', showarrow=False, font=dict(color='grey')))
     if markerTime:
         markerTime = datetime.combine(d.index[0].date(), markerTime.time())
         if markerTime < d.index[0]:
